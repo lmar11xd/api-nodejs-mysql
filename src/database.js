0f0bb5d96f4f9f3165e57b0597bade1e5dcc3187
Luis Miguel Alvarado <lmar11xd@gmail.com>
2022-05-05 19:27:54 -0500

    Configuracion DB

--- src/database.js
+++ src/database.js
@@ -1,10 +1,11 @@
 const mysql = require('mysql');
 
+//mysql://bb52a68e76d5f8:c47cb4d7@us-cdbr-east-05.cleardb.net/heroku_455f6a3a73aa829?reconnect=true
 const mysqlConnection = mysql.createConnection({
-    host: 'https://databases.000webhost.com/',
-    user: 'id18686503_lmar',
-    password: 'Lmar123456789$',
-    database: 'id18686503_seguridadlmar'
+    host: 'us-cdbr-east-05.cleardb.net',
+    user: 'bb52a68e76d5f8',
+    password: 'c47cb4d7',
+    database: 'heroku_455f6a3a73aa829'
 });
 
 mysqlConnection.connect(function (err){
